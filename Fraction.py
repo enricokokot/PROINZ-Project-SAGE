139749d21e26fbd6bdd8cd39abeccff5132c6e1b
coco <kokotenrico@gmail.com>
2022-03-05 08:51:41 +0100

    Remove Introductory Sandbox Files

--- Fraction.py
+++
@@ -1,7 +0,0 @@
-class Fraction:
-    def __init__(self, brojnik, nazivnik=1):
-        self.brojnik = brojnik
-        self.nazivnik = nazivnik
-
-    def print(self):
-        print(str(self.brojnik) + '/' + str(self.nazivnik))
